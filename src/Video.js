3f456e230a95ecadfd40fb25c348560c57f6b73c
AkhileshCM <142301421+AkhileshCM@users.noreply.github.com>
2023-10-16 17:42:16 +0530

    Update Video.js

--- src/Video.js
+++ src/Video.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import "./Videocss.css"
 function Video(props) {
     return (
         <div className='video'>
-            <video height="500px" width="80%" controls >
+            <video id='myVideo' controls >
             <source
-            width="80%"
+           
              src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4" type="video/mp4"></source>
             </video>
         </div>
